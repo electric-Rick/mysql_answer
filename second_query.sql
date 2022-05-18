e5adc4410a301f6aecdb61b770072a291d2fccdc
electric-rick <erick.augusto.ebs@gmail.com>
2022-05-18 16:57:16 -0300

    first commit

---
+++ second_query.sql
@@ -0,0 +1,10 @@
+-- Seoncd Query: Aromatherapy products.
+
+    SELECT grommet_products.product_name, grommet_gifts_categories.category, grommet_products.product_url, grommet_products.product_price_min, grommet_products.product_short_description
+    FROM grommet_product_categories, grommet_products, grommet_gifts_categories, grommet_product_to_keyword, grommet_product_keywords
+    WHERE grommet_products.id LIKE grommet_product_categories.product_id 
+    AND grommet_product_categories.product_category_id=grommet_gifts_categories.id
+    AND grommet_product_to_keyword.product_id=grommet_products.id
+    AND grommet_product_keywords.id=grommet_product_to_keyword.keyword_id
+    AND grommet_products.is_sold_out=0
+    AND grommet_product_keywords.keyword LIKE "%Aromatherapy%";
